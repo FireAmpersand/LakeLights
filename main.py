938b929ce91480b46687643b15292e2ed4d78ed2
Zachary <fireampersand@live.com>
2020-04-27 07:23:12 +0100

    First Push

---
+++ main.py
@@ -0,0 +1,141 @@
+import time
+import threading
+import mysql.connector
+import python.internalLEDs as iLED #Import Inside Class
+#import python.externalLEDs as eLED #Import Outside Class
+
+#Global Vars
+CURRENT_INSIDE_PATTERN = "N/A"
+CURRENT_OUTSIDE_PATTERN = "N/A"
+CURRENT_INSIDE_POWERED_STATE = "N/A"
+CURRENT_OUTSIDE_POWERED_STATE = "N/A"
+INTERNAL_THREAD = ""
+EXTERNAL_THREAD = ""
+
+def callSQL(name):
+
+    mydb = mysql.connector.connect(
+      host="bumblebee.fireampersand.ca",
+      port="10069",
+      user="root",
+      passwd="tester",
+      database="lakerpidb"
+    )
+
+    #Does a call to the sql server for name.
+    if name == "Internal":
+        mycursor = mydb.cursor()
+        mycursor.execute("SELECT * FROM states WHERE Name='Internal'")
+        return mycursor.fetchall()
+    elif name == 'External':
+        mycursor = mydb.cursor()
+        mycursor.execute("SELECT * FROM states WHERE Name='External'")
+        return mycursor.fetchall()
+
+def setState(name, result):
+    #Sets the state of the name strip with the sql result.
+    global CURRENT_INSIDE_PATTERN
+    global CURRENT_OUTSIDE_PATTERN
+    global CURRENT_INSIDE_POWERED_STATE
+    global CURRENT_OUTSIDE_POWERED_STATE
+    if name == 'Internal':
+        CURRENT_INSIDE_PATTERN = result[0][2]
+        CURRENT_INSIDE_POWERED_STATE = result[0][1]
+    elif name == 'External':
+        CURRENT_OUTSIDE_PATTERN = result[0][2]
+        CURRENT_OUTSIDE_POWERED_STATE = result[0][1]
+
+def initialSetup():
+    #Sets up the values on first boot.
+    global CURRENT_INSIDE_PATTERN
+    global CURRENT_OUTSIDE_PATTERN
+    global CURRENT_INSIDE_POWERED_STATE
+    global CURRENT_OUTSIDE_POWERED_STATE
+    result = callSQL("Internal")
+    setState("Internal",result)
+
+    result = callSQL("External")
+    setState("External",result)
+
+
+def needsUpdate(name):
+    #Compare local state vs sql state for name. Returns boolean.
+    result = callSQL(name)
+    if name == 'Internal':
+        if CURRENT_INSIDE_PATTERN != result[0][2] or CURRENT_INSIDE_POWERED_STATE != result[0][1]:
+            return True
+        else:
+            return False
+    elif name == 'External':
+        if CURRENT_OUTSIDE_PATTERN != result[0][2] or CURRENT_OUTSIDE_POWERED_STATE != result[0][1]:
+            return True
+        else:
+            return False
+
+
+def startPattern(name, pattern):
+    if name == 'Internal' and pattern == 'TheaterChase':
+        iLED.runTheater() 
+    elif name == 'External' and pattern == 'TheaterChase':
+        eLED.runTheater()
+
+def modifyThread (name, command):
+    #Change the state of a thread either kill or start
+    global INTERNAL_THREAD
+    global EXTERNAL_THREAD
+    if name == 'Internal':
+        if command == 'start':
+            INTERNAL_THREAD = threading.Thread(target=startPattern, args=(name,CURRENT_INSIDE_PATTERN))
+            INTERNAL_THREAD.daemon = True
+            INTERNAL_THREAD.start()
+        elif command == 'kill':
+            INTERNAL_THREAD.terminate()
+    elif name == 'External':
+        if command == 'start':
+            EXTERNAL_THREAD = threading.Thread(target=startPattern, args=(name,CURRENT_OUTSIDE_PATTERN))
+            EXTERNAL_THREAD.daemon = True
+            EXTERNAL_THREAD.start()
+        elif command == 'kill':
+            EXTERNAL_THREAD.terminate()
+
+def main():
+    #Main function
+    initialSetup()
+
+    #If new threads need to be started, start them
+    if CURRENT_INSIDE_POWERED_STATE == 'ON':
+        modifyThread('Internal', 'start')
+    if CURRENT_OUTSIDE_POWERED_STATE == 'ON':
+        modifyThread('External', 'start')
+
+    #Main Loop
+    while True:
+        time.sleep(5)
+        print(CURRENT_INSIDE_PATTERN + " | " + callSQL('Internal')[0][2])
+        print(needsUpdate('Internal'))
+        #Internal Checks
+        if needsUpdate('Internal') == True:
+            #Stop the old thread if it is running
+            if INTERNAL_THREAD.is_alive() == True:
+                modifyThread('Internal', kill)
+            #Update the new values
+            setState('Internal', callSQL('Internal'))
+
+            #If the new values are on, then start a new thread
+            if CURRENT_INTERNAL_POWERED_STATE == 'ON':
+                modifyThread('Internal', 'start')
+
+        #External Checks
+        if needsUpdate('External') == True:
+            #Stop the old thread if it is running
+            if EXTERNAL_THREAD.is_alive() == True:
+                modifyThread('External', 'kill')
+            #Update new values
+            setState('External', callSQL('External'))
+
+            #If the new values are on, then start a new thread
+            if CURRENT_OUTSIDE_POWERED_STATE == 'ON':
+                modifyThread('External', 'start')
+
+
+main()
---
+++ python/externalLEDs.py
@@ -0,0 +1,126 @@
+import time
+from neopixel import *
+import argparse
+import random
+
+#LED Strip One Setup
+STRIP_LEDS = 472 #Total LEDS For Strip
+STRIP_PIN = 13 #LED Pin For Strip
+STRIP_FREQ = 800000
+STRIP_DMA = 10 #Possible change, maybe switch to 5?
+STRIP_BRIGHTNESS = 127
+STRIP_INVERT = False
+STRIP_CHANNEL = 1
+STRIP = Adafruit_NeoPixel(STRIP_LEDS, STRIP_PIN, STRIP_FREQ, STRIP_DMA, STRIP_INVERT, STRIP_BRIGHTNESS, STRIP_CHANNEL)
+STRIP.begin()
+
+#Master loop varible to keep patterns running
+MASTER_LOOP = False
+
+
+def newBrightness():
+    """Function used to set the Strip's brightness"""
+    STRIP.setBrightness(STRIP_BRIGHTNESS)
+
+def colorwipe(color, wait_ms=1):
+    """Lights up the leds from one end to another with the given color"""
+    for i in range(STRIP.numPixels()):
+        STRIP.setPixelColor(i, color)
+        STRIP.show()
+        time.sleep(wait_ms/1000.0)
+
+def revColorWipe(color, wait_ms=1):
+    """Lights up the leds like colorwipe, but in reverse"""
+    for i in range(STRIP.numPixels()):
+        STRIP.setPixelColor(STRIP.numPixels() - i, color)
+        STRIP.show()
+        time.sleep(wait_ms/1000.0)
+
+
+def pulseColor(color, wait_ms=100):
+    """Turns on all the leds a select color, then turns them all off, making them flash"""
+    for i in range(STRIP.numPixels()):
+        STRIP.setPixelColor(i, color)
+    STRIP.show()
+    time.sleep(wait_ms/1000.0)
+    for t in range(STRIP.numPixels()):
+        STRIP.setPixelColor(t, Color(0,0,0))
+    STRIP.show()
+    time.sleep(wait_ms/1000.0)
+    
+def runRave():
+    """Runs the pulseColor function forever until Master Loop is false"""
+    while MASTER_LOOP == True:
+        pulseColor(Color(0,255,0))
+
+def theaterChase(color,wait_ms=50, times=20):
+    """Function used to create the theater chase animation"""
+    for j in range(times):
+        for q in range(3):
+            for i in range(0, STRIP.numPixels(), 3):
+                STRIP.setPixelColor(i+q, color)
+            STRIP.show()
+            time.sleep(wait_ms/1000.0)
+            for i in range(0, STRIP.numPixels(), 3):
+                STRIP.setPixelColor(i+q, Color(0,0,0))
+                
+def theaterChaseGreen(wait_ms=50, times=20):
+    """Function used to create the theater chase animation"""
+    for j in range(times):
+        for q in range(3):
+            for i in range(0, STRIP.numPixels(), 3):
+                STRIP.setPixelColor(i+q, Color(0,255,0))
+            STRIP.show()
+            time.sleep(wait_ms/1000.0)
+            for i in range(0, STRIP.numPixels(), 3):
+                STRIP.setPixelColor(i+q, Color(0,0,0))
+
+def staticColor(red,green,blue):
+    """Sets all the leds to a given color with the red, green, blue values provided"""
+    for i in range(STRIP.numPixels()):
+        STRIP.setPixelColor(i, Color(red,green,blue))
+    STRIP.show()
+
+def runColorCycle():
+    """Runs a random animation and color forever untill Master Loop is false"""
+    while MASTER_LOOP:
+        type = random.randint(0,1)
+        if type == 0:
+            colorwipe(Color(random.randint(0,255),random.randint(0,255),random.randint(0,255)))
+        if type == 1:
+            theaterChase(Color(random.randint(0,255), random.randint(0,255),random.randint(0,255)))
+        
+
+def startUp():
+    """Function called when the web server first starts to show it is working"""
+    colorwipe(Color(255,0,0))
+    revColorWipe(Color(0,255,0))
+    colorwipe(Color(0,0,255))
+    revColorWipe(Color(0,0,0))
+    pulseColor(Color(0,255,0))
+    pulseColor(Color(0,255,0))
+    pulseColor(Color(0,255,0))
+    pulseColor(Color(0,255,0))
+
+
+def turnOff():
+    """Sets all leds to 'off' (Black)"""
+    colorwipe(Color(0,0,0))
+
+def runCanadaDayAnimation():
+    """Runs two theater chase animations, one red and one white"""
+    while MASTER_LOOP:
+        theaterChase(Color(255,255,255))
+        theaterChase(Color(255,0,0))
+
+def runTheater():
+    """Runs theater chase forever until Master Loop is false"""
+    while MASTER_LOOP:
+        theaterChase(Color(255,255,255))
+        
+
+def runTheaterGreen():
+    """Runs theater chase forever until Master Loop is false"""
+    while MASTER_LOOP:
+        theaterChaseGreen()
+
